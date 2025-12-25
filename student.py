1f4016aa1ab0f03352f848324094784cc6dbda48
ylikhitha04 <ylikhitha04@gmail.com>
2025-12-25 18:17:30 +0530

    Add files via upload

---
+++ student.py
@@ -0,0 +1,15 @@
+class student:
+    def __init__(self):
+        self.name="likhi"
+        self.id=234567
+        self.yos=3
+    def attend(self):
+        print("i am attending calss")
+    def learn(self):
+        print("i am learning")
+    def study(self):
+        print("i an studying")
+s1=student()
+s1.attend()
+s1.learn()
+s1.study()
